23f4ea1a76bb606f3c1d5369540f56c1a830cd10
smy-101 <shimy1011@foxmail.com>
2021-03-23 21:32:26 +0800

    switch组件接受value和input事件

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,8 +1,23 @@
 <template>
-  <button>
+  <button :class="{checked:value}" @click="toggle">
     <span></span>
   </button>
 </template>
+<script lang="ts">
+// import { ref } from 'vue'
+export default {
+  props: {
+    value: Boolean
+  },
+  setup(props, context){
+    const toggle = ()=>{
+      context.emit('input', !props.value)
+      // this.$emit()
+    }
+    return {toggle}
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 $h: 22px;
@@ -11,7 +26,7 @@ button {
   height: $h;
   width: $h*2;
   border: none;
-  background: blue;
+  background: grey;
   border-radius: $h/2;
   position: relative;
 }
@@ -24,8 +39,15 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
-button:hover > span{
+button.checked{
+  background: blue;
+}
+button.checked > span{
   left: calc(100% - #{$h2} - 2px);
+}
+button:focus{
+  outline: none;
 }
 </style>

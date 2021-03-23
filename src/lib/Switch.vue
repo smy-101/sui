d94ecc6184eb835a43a049198545356398d15ffe
smy-101 <shimy1011@foxmail.com>
2021-03-23 22:27:02 +0800

    完成Button组件基本样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="{checked:value}" @click="toggle">
+  <button class="sui-switch" :class="{'sui-checked':value}" @click="toggle">
     <span></span>
   </button>
 </template>
@@ -19,22 +19,22 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
+.sui-switch {
   height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
   > span {
     position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
-  &.checked { background: #1890ff;
+  &.sui-checked { background: #1890ff;
     > span { left: calc(100% - #{$h2} - 2px); }
   }
   &:focus { outline: none; }
   &:active {
     > span { width: $h2 + 4px; }
   }
-  &.checked:active {
+  &.sui-checked:active {
     > span { width: $h2 + 4px; margin-left: -4px; }
   }
 }

819f673eee0eb3b7fe3bfec9f0de39f707d1f751
smy-101 <shimy1011@foxmail.com>
2021-03-30 19:34:11 +0800

    添加Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,39 @@
 <template>
-  <div>Dialog 示例</div>
+  <div>Dialog示例</div>
+  <h1>示例一</h1>
+  <div style="position: relative; z-index: 1;">
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+      <template v-slot:content>
+        <strong>hi</strong>
+        <div>hi2</div>
+      </template>
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+    </Dialog>
+  </div>
+  <div style="position: relative; z-index: 2; width: 300px; height: 300px; background: red;"></div>
 </template>
+
+<script>
+import Dialog from "../lib/Dialog.vue"
+import Button from "../lib/Button.vue";
+import {ref} from "vue";
+
+export default {
+  components: {Dialog, Button},
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {
+    }
+    return {x, toggle, f1, f2}
+  }
+}
+</script>

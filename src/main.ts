5d74f4e89c7919379d8dc38fad48bf6f81f3590f
smy-101 <shimy1011@foxmail.com>
2021-04-01 23:13:33 +0800

    添加组件介绍

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import App from "./App.vue";
 import { router } from './router';
 import 'github-markdown-css'
 import Markdown from "./components/Markdown.vue"
+
+
 const app = createApp(App);
 app.use(router);
 app.mount("#app")

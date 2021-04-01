c06c97d0544293d790ba78b07432fd8f8ab54999
smy-101 <shimy1011@foxmail.com>
2021-04-01 21:41:23 +0800

    增加介绍页面

--- src/router.ts
+++ src/router.ts
@@ -6,8 +6,12 @@ import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
+import Markdown from "./components/Markdown.vue"
+import {h} from "vue"
+
 
 const history = createWebHashHistory();
+const md = filename => h(Markdown,{path:`../markdown/${filename}.md`,key : filename})
 export const router = createRouter({
   history: history,
   routes: [
@@ -17,6 +21,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocDemo },
+        { path: "intro", component:md('intro')  },
+        { path: "get-started", component: md('get-started') },
+        { path: "install", component: md('install') },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
